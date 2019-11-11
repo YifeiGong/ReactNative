53ff660341dd755ac8dd436c01ba208a41e20886
YifeiGong <yifeiaimeegong@hotmail.com>
2019-11-11 13:59:01 -0600

    Vedo set up

--- reactnative/App.js
+++ reactnative/App.js
@@ -1,19 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>11</Text>
-    </View>
-  );
-}
+import CameraPage from './src/camera.page';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default class App extends React.Component {
+    render() {
+        return (
+            <CameraPage />
+        );
+    };
+};
